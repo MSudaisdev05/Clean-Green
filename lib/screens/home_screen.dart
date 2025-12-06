169f087c85621b5ed610aa43327b2827340504b1
Muhammad Sudais <msudais.dev05@gmail.com>
2025-12-06 06:17:03 -0800

    first commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, unused_import, deprecated_member_use
 
 import 'dart:convert';
 import 'package:clean_green_socity/screens/create_post_screen.dart';
